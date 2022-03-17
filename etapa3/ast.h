3da0563357edea83812ad82466d5f5b9318eb8f6
Mayra <mayra.cademartori@gmail.com>
2022-03-16 23:29:15 -0300

    modularization

--- etapa3/ast.h
+++ etapa3/ast.h
@@ -16,3 +16,17 @@ struct LseNodo
     nodo *nodo;
     lseNodo *proximo;
 };
+
+nodo *adiciona_nodo(valorLexico valor_lexico);
+nodo *adiciona_nodo_label(char *label);
+void adiciona_filho(nodo *pai, nodo *filho);
+void imprime_arvore(nodo *nodo, int profundidade);
+lseNodo *acha_ultimo_filho(lseNodo *filhos);
+void adiciona_irmao(lseNodo *irmao, lseNodo *novo_irmao);
+void libera(void *pai);
+void libera_irmaos(void *filhos);
+void libera_nodo(nodo *nodo);
+void libera_valor_lexico(valorLexico valor_lexico);
+void imprime_nodo(nodo *nodo);
+void imprime_arestas(nodo *nodo);
+void exporta(void *arvore);
