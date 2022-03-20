3ac34c237ca2ca1829552fa831843cecc8b152ed
Aline Amorim <ahamorim@inf.ufrgs.br>
2022-03-20 12:10:16 -0300

    esqueleto para etapa 4

---
+++ etapa4/main.c
@@ -0,0 +1,23 @@
+/*
+  Função principal para realização da E3.
+  Não modifique este arquivo.
+*/
+#include <stdio.h>
+#include <string.h>
+extern int yyparse(void);
+extern int yylex_destroy(void);
+
+void *arvore = NULL;
+void exporta (void *arvore);
+void libera (void *arvore);
+
+int main (int argc, char **argv)
+{
+  int ret = yyparse(); 
+  exporta (arvore);
+  imprime_arvore(arvore);
+  libera(arvore);
+  arvore = NULL;
+  yylex_destroy();
+  return ret;
+}
