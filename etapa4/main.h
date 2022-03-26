03079b620540197dbef9fe4eb23ded5c26690789
Mayra <mayra.cademartori@gmail.com>
2022-03-26 00:33:36 -0300

    fix dupla menção "arvore"

--- etapa4/main.h
+++ etapa4/main.h
@@ -1,7 +1,5 @@
 int yyparse(void);
 extern int yyparse(void);
 extern int yylex_destroy(void);
-void *arvore;
 void exporta (void *arvore);
 void libera (void *arvore);
-int main (int argc, char **argv);
